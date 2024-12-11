dcb2fdcc777f68236f1a3298d6dd12a508c1f11d
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2024-12-11 17:09:15 +0800

    rc4

--- NewFrontend/astro.config.mjs
+++ NewFrontend/astro.config.mjs
@@ -3,16 +3,12 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
   prefetch: false,
-  adapter: vercel({
-    webAnalytics: {
-      enabled: false,
-    },
-  }),
+  adapter: vercel(),
 });
