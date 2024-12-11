dffe37d11944689d50018306d0bb58fc527d4e02
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2024-12-11 17:06:03 +0800

    rc3

--- NewFrontend/astro.config.mjs
+++ NewFrontend/astro.config.mjs
@@ -3,19 +3,11 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
-
-import node from "@astrojs/node";
-
-import metaTags from "astro-meta-tags";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sentry(), spotlightjs(), metaTags()],
+  integrations: [react(), tailwind()],
   output: "server",
   prefetch: false,
   adapter: vercel({
