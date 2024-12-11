cd8a1f9bf0067005455815f0d324843d2b6d3e8f
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2024-12-11 17:48:42 +0800

    rc8

--- NewFrontend/src/lib/client.ts
+++ NewFrontend/src/lib/client.ts
@@ -63,5 +63,5 @@ class CustomClient {
 }
 
 export const fetchClient = new CustomClient({
-  baseURL: "http://localhost:3002",
+  baseURL: import.meta.env.BACKEND_URL || "http://localhost:3002",
 });
